9fc1afd501abdcb9b525d8e3a0d417281b465edb
Yash Waghmare <yashwaghmare556@gmail.com>
2024-07-25 01:03:30 +0530

    new

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+// function abc(){
+//     var x = 25;
+//     console.log(x);
+// }
+// abc()
+
+
+// console.log("hello world")
+
+
+const x =  [25, 485, 35]
+// console.log(x)
+
+// x.forEach((item,index)=>{
+//     console.log(item,index)
+// })
+
+// x.map((item)=>{
+//     console.log(item);
+// })
